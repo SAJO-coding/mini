3ce304d22bc0254acada6dcdbf19b94edb74d51c
DagyeongKang <gangda920@gmail.com>
2023-10-18 17:48:26 +0900

    진짜 마지막

--- mini-Project/src/main/java/com/sajo/mini/view/MainView.java
+++ mini-Project/src/main/java/com/sajo/mini/view/MainView.java
@@ -5,6 +5,7 @@ public class MainView {
         System.out.println("테스트 용입니다.");
         System.out.println("테스트 용입니다111111111.");
         System.out.println("내이름으로다시");
+        System.out.println("으아ㅏ아아아아ㅏ");
 
 
     }
