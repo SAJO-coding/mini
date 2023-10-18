fc0adb0ad9039c8958cc221b87f840a772049a10
DagyeongKang <gangda920@gmail.com>
2023-10-18 17:15:45 +0900

    메인뷰 테스트 건드렸습니다.111

--- mini-Project/src/main/java/com/sajo/mini/view/MainView.java
+++ mini-Project/src/main/java/com/sajo/mini/view/MainView.java
@@ -3,7 +3,7 @@ package com.sajo.mini.view;
 public class MainView {
     public static void main(String[] args) {
         System.out.println("테스트 용입니다.");
-        System.out.println("테스트 용입니다11111.");
+        System.out.println("테스트 용입니다111111111.");
 
 
     }
