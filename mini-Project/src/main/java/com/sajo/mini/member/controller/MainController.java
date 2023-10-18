d0578e87447059d47664e71f7867eeff887ef599
DONGROCK <ehdfkr8022@naver.com>
2023-10-18 17:24:48 +0900

    test22

--- mini-Project/src/main/java/com/sajo/mini/member/controller/MainController.java
+++ mini-Project/src/main/java/com/sajo/mini/member/controller/MainController.java
@@ -2,6 +2,6 @@ package com.sajo.mini.member.controller;
 
 public class MainController {
     public static void main(String[] args) {
-        System.out.println("TEST");
+        System.out.println("TESasdasdasdT");
     }
 }
