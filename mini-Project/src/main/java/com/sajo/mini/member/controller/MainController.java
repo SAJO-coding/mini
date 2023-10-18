c2c61f8776492be4f199d876c856025b6cf1a833
DONGROCK <ehdfkr8022@naver.com>
2023-10-18 17:51:36 +0900

    메인컨트롤 수정 한다.

--- mini-Project/src/main/java/com/sajo/mini/member/controller/MainController.java
+++ mini-Project/src/main/java/com/sajo/mini/member/controller/MainController.java
@@ -3,5 +3,6 @@ package com.sajo.mini.member.controller;
 public class MainController {
     public static void main(String[] args) {
         System.out.println("TESasdaadasdasdsdasdT");
+        System.out.println("ㅇㅂㅈㅇㅂㅈ읩ㅈㅇㅈ");
     }
 }
