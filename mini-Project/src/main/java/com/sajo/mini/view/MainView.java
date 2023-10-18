f62fc65f89c8f4deb868965f6cfe09b7110fa1f9
yeonji-0120 <noyeonji43@gmail.com>
2023-10-18 18:46:59 +0900

    Mainview 데스트eragadfd3sdfd

--- mini-Project/src/main/java/com/sajo/mini/view/MainView.java
+++ mini-Project/src/main/java/com/sajo/mini/view/MainView.java
@@ -39,3 +39,6 @@ public class MainView {
 //내꺼 브랜치에서 작업 -> 커밋, 푸시 -> 메인으로 변경 -> git pull -> git merge(origin/yeonji)
 //내꺼 브랜치에서 작업 -> 커밋, 푸시 -> 메인으로 변경 -> git pull -> git merge(origin/yeonji)
 //내꺼 브랜치에서 작업 -> 커밋, 푸시 -> 메인으로 변경 -> git pull -> git merge(origin/yeonji)
+//내꺼 브랜치에서 작업 -> 커밋, 푸시 -> 메인으로 변경 -> git pull -> git merge(origin/yeonji)
+//내꺼 브랜치에서 작업 -> 커밋, 푸시 -> 메인으로 변경 -> git pull -> git merge(origin/yeonji)
+//내꺼 브랜치에서 작업 -> 커밋, 푸시 -> 메인으로 변경 -> git pull -> git merge(origin/yeonji)
