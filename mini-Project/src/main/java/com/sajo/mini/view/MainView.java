825cedb1b7be34018b4c15eced7371e2c4eb560c
kimsung-min <test@testui-MacBookAir.local>
2023-10-18 17:10:33 +0900

    성민 첫 커밋

--- mini-Project/src/main/java/com/sajo/mini/view/MainView.java
+++ mini-Project/src/main/java/com/sajo/mini/view/MainView.java
@@ -1,4 +1,10 @@
 package com.sajo.mini.view;
 
 public class MainView {
+    public static void main(String[] args) {
+        String sum = "sungmin";
+        System.out.println(sum);
+
+
+    }
 }
