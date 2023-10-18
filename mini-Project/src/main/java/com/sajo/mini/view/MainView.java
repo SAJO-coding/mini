22d26e9fc2edbdc9f69578ed65f8f6d7fa852f54
yeonji-0120 <noyeonji43@gmail.com>
2023-10-18 17:59:48 +0900

    Mainview 데스트eragadfd

--- mini-Project/src/main/java/com/sajo/mini/view/MainView.java
+++ mini-Project/src/main/java/com/sajo/mini/view/MainView.java
@@ -2,20 +2,6 @@ package com.sajo.mini.view;
 
 public class MainView {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("테스트 용입니다.");
-        System.out.println("테스트 용입니다111111111.");
 
-
-        System.out.println("내려받기 테스트입니다11.");
-
-
-=======
-
-
-        int num1 = 0;
-        System.out.println(num1);
-        System.out.println(num2);
->>>>>>> origin/yeonji
     }
 }
