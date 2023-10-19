696d8b5ffde497515825c93529417b8fd0128798
kimsung-min <test@testui-MacBookAir.local>
2023-10-19 14:21:52 +0900

    git 연습 2시

--- mini-Project/src/main/java/com/sajo/mini/view/MainView.java
+++ mini-Project/src/main/java/com/sajo/mini/view/MainView.java
@@ -1,5 +1,7 @@
 package com.sajo.mini.view;
 
+import java.sql.DatabaseMetaData;
+
 public class MainView {
     public static void main(String[] args) {
         String sum = "sungmin";
@@ -7,6 +9,12 @@ public class MainView {
         System.out.println("hihi");
         System.out.println("나만 못해 git");
         System.out.println("gg");
+
+        System.out.println("다시 14:20");
+
+
+
+
 
 
     }
