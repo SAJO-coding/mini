d7cfb6882b189cd893448f989840c9ce85b36ef3
DagyeongKang <gangda920@gmail.com>
2023-10-19 18:02:13 +0900

    10.19 6시 2분 강다경 2차 수정

--- mini-Project/src/main/java/com/sajo/mini/run/Application.java
+++ mini-Project/src/main/java/com/sajo/mini/run/Application.java
@@ -1,7 +1,12 @@
 package com.sajo.mini.run;
 
+import com.sajo.mini.model.service.MemberController;
+
 public class Application {
     public static void main(String[] args) {
+
+        MemberController mc = new MemberController();
+        mc.selectNumber();
 
     }
 }
