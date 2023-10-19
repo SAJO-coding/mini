96f97de76fc430c906e43a29486f7d1cc81d2b3a
yeonji-0120 <noyeonji43@gmail.com>
2023-10-19 18:04:08 +0900

    ProductCOntroller 작성

--- mini-Project/src/main/java/com/sajo/mini/member/controller/ProductController.java
+++ mini-Project/src/main/java/com/sajo/mini/member/controller/ProductController.java
@@ -10,7 +10,61 @@ public class ProductController {
     ArrayList<ProductDTO> healerproduct = new ArrayList<>();
     ArrayList<ProductDTO> tankerproduct = new ArrayList<>();
     ArrayList<ProductDTO> dealerlist = new ArrayList<>();
+
+
     Scanner sc = new Scanner(System.in);
+
+    public void selectNumber(){
+        while(true) {
+            System.out.println("===== 메뉴 선택 =====");
+            System.out.println("1. 아이템 등록");
+            System.out.println("2. 아이템 조회");
+            System.out.println("3. 아이템 삭제");
+            System.out.println("4. 종료");
+            int num1 = sc.nextInt();
+            sc.nextLine();
+            switch (num1){
+                case 1:
+                    productRegistration();
+                    break;
+                case 2:
+                    productList();
+                    break;
+                case 3:
+                    productDelete();
+                    break;
+                case 4: return;
+
+            }
+        }
+    }
+
+    private ProductDTO productRegistration() {
+        while (true){
+            System.out.println("===== 아이템 등록 =====");
+            System.out.println("아이템 명을 입력해주세요 : ");
+            String productName = sc.nextLine();
+            System.out.println("등록하시려는 아이템의 직업????을 입력해주세요 : ");
+            System.out.println("힐러 / 탱커 / 딜러");
+            String job = sc.nextLine();
+            System.out.println("아이템의 레벨을 입력해주세요 : ");
+            int levelRestriction = sc.nextInt();
+            System.out.println("아이템의 가격을 입력해주세요 : ");
+            int price = sc.nextInt();
+            System.out.println("아이템 설명을 입력해주세요 : ");
+            String demonstration = sc.nextLine();
+            ProductDTO product = new ProductDTO(productName, job, levelRestriction, price, demonstration);
+
+            return product;
+        }
+    }
+
+    private void productList() {
+        System.out.println("------------ 등록된 아이템 ------------");
+    }
+
+    private void productDelete() {
+    }
 
     public List<ProductDTO> butProduct(int answer){
         ArrayList<ProductDTO> wantBuy = new ArrayList<>();
--- mini-Project/src/main/java/com/sajo/mini/model/dto/ProductDTO.java
+++ mini-Project/src/main/java/com/sajo/mini/model/dto/ProductDTO.java
@@ -6,6 +6,66 @@ public class ProductDTO {
     int price;
     int levelRestriction;
     String demonstration;
-    {
+
+    public ProductDTO() {
+    }
+
+    public ProductDTO(String productName, String job, int price, int levelRestriction, String demonstration) {
+        ProductName = productName;
+        Job = job;
+        this.price = price;
+        this.levelRestriction = levelRestriction;
+        this.demonstration = demonstration;
+    }
+
+    public String getProductName() {
+        return ProductName;
+    }
+
+    public void setProductName(String productName) {
+        ProductName = productName;
+    }
+
+    public String getJob() {
+        return Job;
+    }
+
+    public void setJob(String job) {
+        Job = job;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getLevelRestriction() {
+        return levelRestriction;
+    }
+
+    public void setLevelRestriction(int levelRestriction) {
+        this.levelRestriction = levelRestriction;
+    }
+
+    public String getDemonstration() {
+        return demonstration;
+    }
+
+    public void setDemonstration(String demonstration) {
+        this.demonstration = demonstration;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductDTO{" +
+                "ProductName='" + ProductName + '\'' +
+                ", Job='" + Job + '\'' +
+                ", price=" + price +
+                ", levelRestriction=" + levelRestriction +
+                ", demonstration='" + demonstration + '\'' +
+                '}';
     }
 }
