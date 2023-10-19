71bc2b2e9b662493314039d685b6c52c6118bcae
yunha-cha <helena0228c@gmail.com>
2023-10-19 15:56:52 +0900

    2023-10-19 MainController 클래스에 메소드 테스트코드 추가

--- mini-Project/src/main/java/com/sajo/mini/member/controller/MainController.java
+++ mini-Project/src/main/java/com/sajo/mini/member/controller/MainController.java
@@ -18,7 +18,7 @@ public class MainController {
     public void testMethod() {
 
         System.out.println("test입니다.");
-        System.out.println("test입니다.");
-        System.out.println("test입니다.");
+        System.out.println("test33333입니다.");
+        System.out.println("test44444입니다.");
     }
 }
