95b67d2e323d87237bca47005cd87d74dd2ebc6f
yeonji-0120 <noyeonji43@gmail.com>
2023-10-19 17:31:32 +0900

    selectNumber 메소드 작성

--- mini-Project/src/main/java/com/sajo/mini/member/controller/ProductController.java
+++ mini-Project/src/main/java/com/sajo/mini/member/controller/ProductController.java
@@ -1,4 +1,39 @@
 package com.sajo.mini.member.controller;
 
+import com.sajo.mini.product.Product;
+
+import java.util.Scanner;
+
 public class ProductController {
+    Scanner sc = new Scanner(System.in);
+    Product product = new Product();
+    private void selectNumber() {
+        while (true) {
+            System.out.println("===== 메뉴 =====");
+            System.out.println("1. 아이템 등록");
+            System.out.println("2. 아이템 조회");
+            System.out.println("3. 아이템 삭제");
+            System.out.println("4. 종료");
+            int num1 = sc.nextInt();
+            sc.nextLine();
+            switch (num1){
+                case 1: productRegistration(); break;
+                case 2: productList(); break;
+                case 3: produceDelete(); break;
+                case 4: return;
+            }
+        }
+    }
+
+    public void productRegistration(){
+
+    }
+
+    public void productList(){
+
+    }
+
+    public void produceDelete(){
+
+    }
 }
--- mini-Project/src/main/java/com/sajo/mini/product/Product.java
+++ mini-Project/src/main/java/com/sajo/mini/product/Product.java
@@ -1,4 +1,71 @@
 package com.sajo.mini.product;
 
 public class Product {
+    private String productName;
+    private String job;
+    private int price;
+    private int levelRestriction;
+    private String demonstration;
+
+    public Product() {
+    }
+
+    public Product(String productName, String job, int price, int levelRestriction, String demonstration) {
+        this.productName = productName;
+        this.job = job;
+        this.price = price;
+        this.levelRestriction = levelRestriction;
+        this.demonstration = demonstration;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getLevelRestriction() {
+        return levelRestriction;
+    }
+
+    public void setLevelRestriction(int levelRestriction) {
+        this.levelRestriction = levelRestriction;
+    }
+
+    public String getDemonstration() {
+        return demonstration;
+    }
+
+    public void setDemonstration(String demonstration) {
+        this.demonstration = demonstration;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productName='" + productName + '\'' +
+                ", job='" + job + '\'' +
+                ", price=" + price +
+                ", levelRestriction=" + levelRestriction +
+                ", demonstration='" + demonstration + '\'' +
+                '}';
+    }
 }
