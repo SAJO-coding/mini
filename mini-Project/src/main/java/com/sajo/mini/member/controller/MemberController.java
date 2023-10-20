688a0851a8a69d1151a2b8cc472adbb60d3cc39d
DagyeongKang <gangda920@gmail.com>
2023-10-20 17:17:27 +0900

    10.20 17시 17분 강다경 맨처음 로그인 시 디테일 수정

--- mini-Project/src/main/java/com/sajo/mini/member/controller/MemberController.java
+++ mini-Project/src/main/java/com/sajo/mini/member/controller/MemberController.java
@@ -6,14 +6,14 @@ import java.util.Scanner;
 
 public class MemberController {
     /* 시작 정보입력
-    *  사용자 서비스 */
-        Scanner sc = new Scanner(System.in);
-        static MemberDTO memberDTO;
-        MemberManagerController mmc = new MemberManagerController();
-        ProductController pc = new ProductController();
+     *  사용자 서비스 */
+    Scanner sc = new Scanner(System.in);
+    static MemberDTO memberDTO;
+    MemberManagerController mmc = new MemberManagerController();
+    ProductController pc = new ProductController();
 
 
-    public void selectNumber(){
+    public void insertInformation() {
 
         System.out.print("닉네임을 입력해주세요 : ");
         String name = sc.nextLine();
@@ -23,42 +23,58 @@ public class MemberController {
         System.out.print("직업을 입력해주세요 (힐러,탱커,딜러 중 선택): ");
         String job = sc.nextLine();
 
-        memberDTO = new MemberDTO(name,job,level,mmc.goldCaculator(level,job));
+        memberDTO = new MemberDTO(name, job, level, mmc.goldCaculator(level, job));
 
-        while (true){
-            System.out.println();
-            System.out.println("===선택지===");
-            System.out.println("1. 물건 보기");
-            System.out.println("2. 장바구니 확인");
-            System.out.println("3. 구매하기");
-            System.out.println("9. 프로그램 종료");
-            System.out.print("번호를 입력해주세요 : ");
+        selectNumber();
 
-            int answer = sc.nextInt();
-            switch (answer){
-                case 1 : productCategory();
-                    break;
-                case 2 : mmc.cartPrint();
-                    break;
-                case 3 : buy();
-                    break;
-                case 9 :
-                    return;
-                default:
-                    System.out.println("존재하는 번호가 아닙니다.");
-                    System.out.println("다시 입력해주세요");
-                    break;
-
-            }
-        }
 
     }
+
+    public void selectNumber() {
+
+
+        while(true)
+
+    {
+        System.out.println();
+        System.out.println("===선택지===");
+        System.out.println("1. 물건 보기");
+        System.out.println("2. 장바구니 확인");
+        System.out.println("3. 구매하기");
+        System.out.println("9. 프로그램 종료");
+        System.out.print("번호를 입력해주세요 : ");
+
+        int answer = sc.nextInt();
+        switch (answer) {
+            case 1:
+                productCategory();
+                break;
+            case 2:
+                mmc.cartPrint();
+                break;
+            case 3:
+                buy();
+                break;
+            case 9:
+                return;
+            default:
+                System.out.println("존재하는 번호가 아닙니다.");
+                System.out.println("다시 입력해주세요");
+                break;
+
+        }
+    }
+
+}
+
+
 
     public void productCategory(){
         while (true){
             System.out.println("1. 힐러");
             System.out.println("2. 탱커");
             System.out.println("3. 딜러");
+            System.out.println("9. 물건 구매 종료");
             System.out.print("원하시는 아이템 카테고리의 번호를 입력해주세요 : ");
             int answer =sc.nextInt();
 
--- mini-Project/src/main/java/com/sajo/mini/run/Application.java
+++ mini-Project/src/main/java/com/sajo/mini/run/Application.java
@@ -3,7 +3,6 @@ package com.sajo.mini.run;
 import com.sajo.mini.member.controller.MemberController;
 import com.sajo.mini.member.controller.MemberManagerController;
 import com.sajo.mini.member.controller.ProductController;
-import com.sajo.mini.model.dto.ItemDTO;
 
 import java.util.Scanner;
 
@@ -11,28 +10,19 @@ public class Application {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        MemberController mc = new MemberController();
-        MemberManagerController mmc = new MemberManagerController();
 
-        ProductController pc = new ProductController();
-
-        //아이템 객체 배열
-        ItemDTO itemDTO1 = new ItemDTO("갑옷1", "딜러", 2000, 5, "방어력 +10");
-        ItemDTO itemDTO2 = new ItemDTO("신발1", "딜러", 3000, 8, "속도 +10");
-        ItemDTO itemDTO3 = new ItemDTO("바지1", "딜러", 5000, 4, "힐 +10");
-
-//        mmc.itemCart(item3);
-
-        System.out.println("이용할 서비스를 선택해주세요");
         System.out.println("1. 상인관리페이지");
         System.out.println("2. 구매자 페이지");
+        System.out.print("이용할 서비스를 선택해주세요 : ");
         int num1 = sc.nextInt();
         switch (num1){
             case 1 :
+                ProductController pc = new ProductController();
                 pc.selectNumber();
                 break;
             case 2 :
-                mc.selectNumber();
+                MemberController mc = new MemberController();
+                mc.insertInformation();
                 break;
         }
     }
